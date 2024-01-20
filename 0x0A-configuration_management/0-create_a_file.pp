6b5aa70d8f51158bd99844ea1fdc96130fc99dff
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-01-20 03:54:53 +0000

    Modified: for the ensure attribute

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
 # Creating a temp file in /tmp.
 file{'/tmp/school':
-  owner   => "www-data",
-  group   => "www-data",
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
-  mode    => '0744'
+  
 }
