b48e1431a25ac541132af14d8fad2315cc6836c9
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-01-20 04:29:29 +0000

    Puppet linter

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
-# puppet file to execute a commnad.
+# a manifest that kills a process named killmenow
 
 exec { 'pkill':
-  path     => '/usr/bin, /sbin, /bin, /user/sbin',
-  command  => 'pkill -f killmenow',
-  provider => 'shell',
-}
+path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+command  => 'pkill -f killmenow',
+provider => 'shell',}
