9e30866aba72a840add46f7a4925a55745ebb33a
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-01-24 23:37:34 +0000

    Modified

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,20 @@
-# Setting the ssh configuration file
-exec { 'configuration_file':
-  command => 'sed -i "s|PasswordAuthentication yes|PasswordAuthentication no", /etc/ssh/ssh_config',
-  path    => '/bin',
+# ssh_config_manifest.pp
+
+# Ensure the SSH client configuration directory exists
+file { '/home/ubuntu/.ssh':
+  ensure  => 'directory',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0700',
+  require => File['/home/ubuntu'],
+}
+
+# Configure SSH client to use the private key and refuse password authentication
+file { '/home/ubuntu/.ssh/config':
+  ensure  => 'file',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0600',
+  content => "Host 100.26.216.148\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",
+  require => File['/home/ubuntu/.ssh'],
 }
