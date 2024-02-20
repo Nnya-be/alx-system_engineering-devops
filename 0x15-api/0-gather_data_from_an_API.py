14e5a791542c79879531795a9e2ba1c8b5642277
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-02-20 03:09:01 +0000

    Modified: for the correct format

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
 import requests
-import sys
+from sys import argv
 
 """A script to get information about a person to do lists."""
 
 if __name__ == '__main__':
-    api_url = f'https://jsonplaceholder.typicode.com/todos/{sys.argv[1]}'
-    try:
-        response = requests.get(api_url)
-        response.raise_for_status()
-        todos = response.json()
-
-        employee_name = todos[0]['username']
-        total_tasks = len(todos)
-        completed_tasks = sum(todo['completed'] for todo in todos)
-
-        print(f'Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):')
-        print(f'\t{employee_name}: {completed_tasks}/{total_tasks}')
-
-    except requests.RequestException:
-        print(f'An error occured while fetching the data')
+    todos = f'https://jsonplaceholder.typicode.com/todos?userId={argv[1]}'
+    user = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
+    tasks = requests.get(todos).json()
+    users = requests.get(user).json()
+    task_title = [task['title'] for task in tasks if task.get('completed')]
+    print(f"Employee {users['name']} is done with tasks ({len(task_title)}/{len(tasks)}:")
+    for title in task_title:
+        print(f'\t{title}')
