c373e6b7376a7eede8acb3b98689eef097a7e148
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-02-20 03:21:18 +0000

    Modified: pycodestyle and pydocstyelsyles

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+"""A script to get information about a person to do lists."""
 import requests
 from sys import argv
 
-"""A script to get information about a person to do lists."""
 
 if __name__ == '__main__':
     todos = f'https://jsonplaceholder.typicode.com/todos?userId={argv[1]}'
     user = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
-    tasks = requests.get(todos).json()
-    users = requests.get(user).json()
-    task_title = [task['title'] for task in tasks if task.get('completed')]
-    print(f"Employee {users['name']} is done with tasks ({len(task_title)}/{len(tasks)}:")
-    for title in task_title:
+    tsk = requests.get(todos).json()
+    uses = requests.get(user).json()
+    tsk_t = [tsk['title'] for task in tsk if task.get('completed')]
+    m = f"Employee {uses['name']} is done with tasks ({len(tsk_t)}/{len(tsk)}:"
+    print(m)
+    for title in task_t:
         print(f'\t{title}')
