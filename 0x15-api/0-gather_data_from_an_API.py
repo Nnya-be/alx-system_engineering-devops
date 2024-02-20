1eadc666648de61b904f6da815c1b7e3364fadac
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-02-20 03:25:10 +0000

    Modified: To make the code consistent

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     user = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
     tsk = requests.get(todos).json()
     uses = requests.get(user).json()
-    tsk_t = [tsk['title'] for task in tsk if task.get('completed')]
+    tsk_t = [task['title'] for task in tsk if task.get('completed')]
     m = f"Employee {uses['name']} is done with tasks ({len(tsk_t)}/{len(tsk)}:"
     print(m)
-    for title in task_t:
+    for title in tsk_t:
         print(f'\t{title}')
