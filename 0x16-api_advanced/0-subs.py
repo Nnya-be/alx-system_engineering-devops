be3ab28db253d263025d6e2ede52bb9a4db974e6
Solomon Chamamme Nnyabe <solomonchamamme@gmail.com>
2024-03-22 17:38:57 +0000

    Modified

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """Returns the number of subscribers for a given subreddit."""
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
     """Return the number of subscribers."""
-    url = 'https://www.reddit.com/r/{}/about.json'
-    header = {'user-agent': 'myapp'}
-    r = requests.get(url.format(subreddit), headers=header)
-    if r.status_code == 404:
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    header = {'User-Agent': 'myapp'}
+    response = requests.get(url, headers=header)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
-    return (r.json().get('data').get('subscribers'))
